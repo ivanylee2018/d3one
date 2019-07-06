791450e2d9bf368c5eee8c29c36dc7b054ff8f33
ivanylee2018 <38091005+ivanylee2018@users.noreply.github.com>
2019-07-05 17:00:44 -0700

    working api
    this code has the working api to chart.js

--- charts/urls.py
+++ charts/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
 from django.conf.urls import url
-from .views import home, play_count_by_month
+from .views import home, get_data, ChartData
 
 urlpatterns = [
     path('', views.home, name='charts-home'),
     path('about', views.about, name='charts-about'),
     url(r'^$', home),
-    url(r'^api/play_count_by_month', play_count_by_month, name='play_count_by_month'),
+    url(r'^api/data/$', get_data, name='api-data'),
+    url(r'^api/chart/data/$', ChartData.as_view()),
 ]
 
 
--- charts/views.py
+++ charts/views.py
@@ -1,9 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db import connections
 from django.db.models import Count
 from django.http import JsonResponse
 from .models import Play
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
+User = get_user_model()
 
 def home(request):
     return render(request, 'charts/home.html')
@@ -11,12 +16,36 @@ def home(request):
 def about(request):
     return render(request,'charts/about.html')
 
-def play_count_by_month(request):
-    data = Play.objects.all()\
-        .extra(select={'month': connections[Play.objects.db].ops.date_trunc_sql('month', 'date')}) \
-        .values('month')\
-        .annotate(count_items=Count('id'))
-    return JsonResponse(list(data), safe=False)
+def get_data(request):
+    data ={
+        "sales": 100,
+        "customers": 10,
+    }
+    return JsonResponse(data)
+
+
+
+class ChartData(APIView):
+    authentication_classes = []
+    permission_classes = []
+
+    def get(self, request, format=None):
+
+        labels = ['Users', 'Blue', 'Yellow', 'Green']
+
+        #getting data from db and making it to a list
+        num = Play.objects.all().values_list('name', flat=True)
+
+
+        data = {
+            "labels": labels,
+            "default": num,
+        }
+
+        return Response(data)
+
+
+
 
 
 
